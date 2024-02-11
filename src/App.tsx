e5b863f868fc204589cd5268679d717873c46df6
toby7002 <144540995+toby7002@users.noreply.github.com>
2024-02-11 21:11:12 +0700

    Hmmm

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,31 @@ import { createSignal } from "solid-js";
 
 export default function () {
   return (
-    <div>
-      <p class="text-center">I'm building it</p>
+    <div class="w-screen h-screen">
+    <div class="grid place-items-center h-full">
+      <div class="card w-96 bg-neutral shadow-xl hover:scale-105 duration-300">
+        <figure class="px-10 pt-10">
+          <img
+            src="https://avatars.githubusercontent.com/u/144540995"
+            alt="avatar"
+            class="rounded-full"
+          />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Nguyễn Minh Thọ (Toby)</h2>
+          <p>TypeScript, PHP, Kotlin Developer</p>
+          <div class="card-actions">
+            <a
+              href={`https://github.com/toby7002`}
+              target="_blank"
+              class="h-auto w-6"
+            >
+              <i class="fa-brands fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
